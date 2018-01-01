9f6298ab3366945f80a793ba55292e9778bf0fdb
Abhishek Jaguwala <abhishekjaguwala@Abhisheks-MacBook-Air.local>
2018-01-01 17:48:31 +0530

    Firebase Work Done

---
+++ DDITIAN/Controller/DDITIANVC.swift
@@ -0,0 +1,23 @@
+//
+//  DDITIANVC.swift
+//  DDITIAN
+//
+//  Created by Abhishek Jaguwala on 30/12/17.
+//  Copyright © 2017 Abhishek Jaguwala. All rights reserved.
+//
+
+import UIKit
+
+
+
+
+class DDITIANVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+}
+
---
+++ Pods/Target Support Files/Pods-DDITIAN/Pods-DDITIAN-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DDITIAN : NSObject
+@end
+@implementation PodsDummy_Pods_DDITIAN
+@end
